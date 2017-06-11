e2f1a612613b3e97e8d93d771f4bd78bc87d298e
NENO7 <db559282@mailna.co>
2017-06-11 18:48:29 +0300

    Create lock_emoji.lua

--- plugins/lock_emoji.lua
+++ plugins/lock_emoji.lua
@@ -1,10 +1,9 @@
---[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
+--[[ @KNSLTHM
+@KNSLTHM
+@KNSLTHM
+@NENO_CH
+@NENO_CH
+@NENO_CH
 --]]
 local function run(msg, matches)
     if is_momod(msg) then
@@ -189,11 +188,10 @@ return {patterns = {
  "🖐🏼"
 },run = run}
 
---[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
+--[[ @KNSLTHM
+@KNSLTHM
+@KNSLTHM
+@NENO_CH
+@NENO_CH
+@NENO_CH
 --]]
