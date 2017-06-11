7d49ae85cb397f9ca48f377351d94b9a7504a061
NENO7 <db559282@mailna.co>
2017-06-11 18:01:31 +0300

    Create all.lua

--- plugins/all.lua
+++ plugins/all.lua
@@ -1,9 +1,9 @@
---[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
+--[[ @KNSLTHM
+@KNSLTHM
+@KNSLTHM
+@NENO_CH
+@NENO_CH
+@NENO_CH
 --]]
 do
 local function get_msgs_user_chat(user_id, chat_id)
@@ -152,4 +152,4 @@ return {
   run = run
 }
 end
--- arabic : @mohammedzedan
+-- arabic : @KNSLTHM
