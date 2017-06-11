3d24503e13ed4f2c564f3e3306fbc798b03b2696
NENO7 <db559282@mailna.co>
2017-06-11 18:00:33 +0300

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -1,9 +1,9 @@
---[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
+--[[ @KNSLTHM
+@KNSLTHM
+@KNSLTHM
+@NENO_CH
+@NENO_CH
+@NENO_CH
 --]]
 
 local function set_bot_photo(msg, success, result)
