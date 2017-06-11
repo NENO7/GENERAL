5315e0662937a51a734dfde2bcf25a25cfb73982
NENO7 <db559282@mailna.co>
2017-06-11 18:51:15 +0300

    Create lock_inlin.lua

--- plugins/lock_inlin.lua
+++ plugins/lock_inlin.lua
@@ -1,10 +1,9 @@
---[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
+--[[ @KNSLTHM
+@KNSLTHM
+@KNSLTHM
+@NENO_CH
+@NENO_CH
+@NENO_CH
 --]]
 do
 local function pre_process(msg)
@@ -40,11 +39,10 @@ pre_process = pre_process
 }
 end
 
---[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
+--[[ @KNSLTHM
+@KNSLTHM
+@KNSLTHM
+@NENO_CH
+@NENO_CH
+@NENO_CH
 --]]
